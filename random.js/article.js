a2adafa8da3f3fcd1c6b7d1d0bc3630e386edfdf
Giang Ngo <valegs137@gmail.com>
2024-04-15 18:03:34 +0700

    update all

--- random.js/article.js
+++ random.js/article.js
@@ -1,9 +1,1 @@
-var img = document.getElementById('image'); 
-var container = document.getElementById('container');
-//or however you get a handle to the IMG
-var img_width = img.clientWidth;
-var img_height = img.clientHeight;
-var container_width = container.clientWidth;
-if (img_width>container_width){
-    img_width = container_width;
-}
+
