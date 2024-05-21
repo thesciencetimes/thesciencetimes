3d26418122731332fb70d7afd33561d609fa8038
Giang Ngo <valegs137@gmail.com>
2024-05-21 17:43:05 +0700

    fix parsing error
    fix parsing error temporarily

--- sci.js
+++ sci.js
@@ -1,9 +1,2 @@
-function show(x) {
-    x.classList.toggle("change");
-    if (document.getElementById("list").style.display== "block") {
-        document.getElementById("list").style.display = "none";
-    }
-    else {
-        document.getElementById("list").style.display = "block";
-    }
-}
+const table_ofcontent = document.querySelector('.container');
+table_ofcontent.addEventListener('click', moveUp);
