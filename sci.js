f43f29b49a42fd18b692f3edb82648015791df52
Giang Ngo <valegs137@gmail.com>
2024-05-21 17:28:52 +0700

    update table  of contents
    update table of contents template, and for alzhe-jil and h20-bk

---
+++ sci.js
@@ -0,0 +1,9 @@
+function show(x) {
+    x.classList.toggle("change");
+    if (document.getElementById("list").style.display== "block") {
+        document.getElementById("list").style.display = "none";
+    }
+    else {
+        document.getElementById("list").style.display = "block";
+    }
+}
