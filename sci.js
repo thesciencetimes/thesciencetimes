422029f6e9dea8bca5691146b33f19d6e0c1ef99
Giang Ngo <valegs137@gmail.com>
2024-05-22 10:53:25 +0700

    Update sci.js

--- sci.js
+++ sci.js
@@ -12,7 +12,10 @@ function open(x){
     x.classList.toggle("change");
 }
 
-if (window.innerWidth<1036) {
-    ​$('topnav').css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'none');​​​​​​
-    ​$('container1').css('display'​​​​​​​​​​​​​​​​​​​​​​​​​​​,'block');​​​​​​
+if (window.innerWidth < 1000) {
+    document.getElementById("container1").style.display="block";
+    var elems = document.getElementsByClassName("topnav");
+    for (var i = 0; i < elems.length; i += 1) {
+        elems[i].style.display = 'none';
+    }
 }
