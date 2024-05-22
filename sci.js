b85216f9c834f16e3eae1af4de6b7f54e0e438a0
Giang Ngo <valegs137@gmail.com>
2024-05-22 10:43:33 +0700

    sci.js update header
    sci.js update all header

--- sci.js
+++ sci.js
@@ -1,7 +1,6 @@
-
 function show(x) {
     x.classList.toggle("change");
-    if (document.getElementById("list").style.display== "block") {
+    if (document.getElementById("list").style.display == "block") {
         document.getElementById("list").style.display = "none";
     }
     else {
@@ -9,3 +8,14 @@ function show(x) {
     }
 }
 
+function open(x){
+    x.classList.toggle("change");
+}
+
+if (window.innerWidth<1036) {
+    document.getElementById("container1").style.display="block";
+    var elems = document.getElementsByClassName("topnav");
+    for (var i = 0; i < elems.length; i += 1) {
+        elems[i].style.display = 'none';
+    }
+}
